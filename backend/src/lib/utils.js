2688d3bd269bd01cbd3c7bab9766ebfc3dfc769e
drxlzz <markbarta5@gmail.com>
2025-11-15 15:55:32 +0100

    signup controller added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  // create a token for the user
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  return token;
+};
+
+// http://localhost
+// https://somedomain.com - s stands for secure
